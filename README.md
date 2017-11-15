af19e47046d66f3b889726acba5d324892c441ee
mos <acwzy@live.com>
2017-11-15 22:40:44 +0800

    finish base feature

--- README.md
+++ README.md
@@ -4,7 +4,87 @@
 inline your script when using webpack requiring html-webpack-plugin
 
 
-require node 6.0 or above
+#### Requirements
+* node >= 6.0 with npm
+* html-webpack-plugin
 
+
+#### Installation
+```shell
+
+npm install inline-script-webpack-plugin --save-dev
+
+#or
+
+npm i inline-script-webpack-plugin -D
+
+```
+
+#### Usage:
+
+for webpack.config.js:
+```javascript
+
+//single script
+module.exports = {
+  entry: {
+  //...
+    inline: []
+  },
+
+  //...
+  plugins: [
+    //...
+    new HtmlWebpackPlugin(),
+    new InlineScriptWebpackPlugin(), //default chunk name will be   `inline` if no name option is given
+    //...
+  ]
+}
+
+
+//multi scripts and custom entry
+module.exports = {
+  entry: {
+    //...
+    foo: [],
+    bar: []
+  },
+
+  //...
+  plugins: [
+    //...
+    new HtmlWebpackPlugin(),
+    new InlineScriptWebpackPlugin({
+      names:['foo', 'bar']
+    }), //multi scripts require names options
+    //...
+  ]
+}
+
+```
+
+for index.html or template:
+
+```html
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Template</title>
+    <!--multi and ordered-->
+    <%=htmlWebpackPlugin.files.foo%>
+    <%=htmlWebpackPlugin.files.bar%>
+</head>
+<body>
+    <!--single-->
+    <%=htmlWebpackPlugin.files.inline%>
+</body>
+</html>
+```
 
 not available yet
+
+TODO:  
+[  ] name(s) will support regex and entry name
--- index.js
+++ index.js
@@ -10,21 +10,53 @@ module.exports = class InlineScriptWebpackPlugin {
       compilation.plugin('html-webpack-plugin-before-html-generation', (htmlPluginData, callback) => {
         const name = this.options.name
         const names = this.options.names
+        let inlines = []
+        const scripts = []
         if (name && names) {
           throw new Error('[InlineScriptWebpackPlugin]: name is invalid with names')
         } else if (name) {
-
+          if (/^[a-zA-Z]/g.test(name)) {
+            inlines.push(name)
+          } else {
+            throw new Error('[InlineScriptWebpackPlugin]: value type of name option must be string')
+          }
         } else if (names) {
-
+          if (names instanceof Array) {
+            inlines = [].concat(names)
+          } else {
+            throw new Error('[InlineScriptWebpackPlugin]: value type of names option must be array')
+          }
         } else {
-          throw new Error('[InlineScriptWebpackPlugin]: a name should be given at least')
+          inlines.push('inline')
         }
 
 
         const assets = htmlPluginData.assets
+        const inlineChunks = compilation.chunks.filter(chunk => inlines.indexOf(chunk.name) > -1) || []
 
+        if (inlineChunks.length > 0) {
+          for (let inlineChunk of inlineChunks) {
+            let file = inlineChunk.files[0]
+            if (compilation.assets[file]) {
+              let asset = compilation.assets[file]
+              scripts.push({name: inlineChunk.name, source: `<script>${asset.source()}</script>`})
+              let index = assets.js.indexOf(assets.publicPath + file)
+              if (index >= 0) {
+                assets.js.splice(index, 1)
+                delete assets.chunks.manifest
+              }
+            }
 
-        callback && callback()
+          }
+        }
+
+        if (scripts.length > 0) {
+          for (let script of scripts) {
+            let name = script.name === 'manifest' ? 'webpackManifest' : script.name
+            assets[name] = script.source
+          }
+        }
+        callback && callback(null, htmlPluginData)
       })
     })
   }
